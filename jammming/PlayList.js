f66fcb0cb17791139c851be2fd69b45a9dcb2178
Suzana Jelinic <suz_ana.j@outlook.com>
2024-04-29 04:09:08 -0400

    playlistRenaming

--- jammming/PlayList.js
+++ jammming/PlayList.js
@@ -3,14 +3,41 @@ import TrackList from './TrackList';
 
 class PlayList extends 
 React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isEditing: false,
+            newName: props.playlistName
+        };
+    }
+
+    handleInputChange = (event) => {
+        this.setState({ newName: event.target.value });
+    };
+
+    handleTitleClick = () => {
+        this.setState({ isEditing: true });
+    };
+
+    handleTitleBlue = () => {
+        this.setState({ isEditing: false });
+
+        this.props.onNameChange(this.state.newName);
+    };
+
     render() {
         return (
             <div className="PlayList">
+                {this.state.isEditing ? (
                 <input 
-            value={this.props.playlistName}
-            onChange={this.props.onNameChange}
+           value={this.state.newName}
+           onChange={this.handleInputChange}
+           onBlur={this.handleTitleBlur}
+                        autofocus
             /> 
-                <h2>Playlist</h2>
+                ) : (
+                <h2 
+            onClick={this.handleTitleClick}>{this.props.playlistName}</h2> )}
                 <TrackList 
             tracks={this.props.playlistTracks}
             onRemove={this.props.onRemove}
