f28653745595e4a03f207e4b48e1cbc529e8cc02
Suzana Jelinic <suz_ana.j@outlook.com>
2024-04-29 11:09:44 -0400

    Tracklist.css

--- jammming/TrackList.js
+++ jammming/TrackList.js
@@ -1,25 +1,20 @@
 import React from 'react';
 import Track from './Track';
+import './TrackList.css'
 
-class TrackList extends
-React.Component {
-    render() {
-        return (
-            <div className="TrackList">
-                {this.props.tracks.map(track => (
-                    <Track
-                        key={track.id}
-                        name={track.name}
-                        artist={track.artist}
-                        album={track.album}
-                onAdd={this.props.onAdd}
-                isRemoval={this.props.isRemoval}
-                    />
-                ))}
-                
-            </div>
-        );
-    }
-}
-
-export default TrackList;
+function TrackList({ tracks, onRemoveTrack, onAddTrack }) {
+    return (
+      <div className="TrackList">
+        {tracks.map(track => (
+          <div className="Track" key={track.id}>
+            <h3>{track.name}</h3>
+            <p>{track.artist} | {track.album}</p>
+            <button className="Track-action" onClick={() => onAddTrack(track)}>+</button>
+            <button className="Track-action" onClick={() => onRemoveTrack(track)}>−</button>
+          </div>
+        ))}
+      </div>
+    );
+  }
+  
+  export default TrackList; 
